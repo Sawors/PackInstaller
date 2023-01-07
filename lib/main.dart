59d63f13c13e1a489d39054f221c949b882a4b4c
Sawors <yt.sawors@gmail.com>
2023-01-07 22:02:08 +0100

    learning Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
-/*import 'package:flutter/material.dart';*/
-import 'modpack_installer/worker/pack_downloader.dart';
-
-
-/*import 'dog_card.dart';
-import 'dog_model.dart';*/
+import 'package:flutter/material.dart';
+import 'package:random_string/random_string.dart';
 
 void main(List<String> args) {
-  //runApp(const MyApp());
+  runApp(const MainApp());
+
+
+
+  //
+  // For the pack-updater backend
+  /*
   ProfileManager.init();
   // tiboise : https://github.com/Sawors/1.16.5-Tiboise/archive/refs/heads/main.zip
   String dlLink = args.isNotEmpty ? args[0] : "https://github.com/Sawors/PackInstaller/raw/main/lib/modpack_installer/sample_modpack/sample_modpack.zip";
@@ -15,66 +17,83 @@ void main(List<String> args) {
   const String root = "C:\\Users\\sosol\\AppData\\Roaming\\.minecraft\\.profiles";
   //PackUpdater.getUpdate(Directory("$root\\Sample 1.16.5"), Directory("$root\\Sample 1.16.5 2"));
   PackInstaller.setupModpack(Uri.parse(dlLink));
+  */
 }
 
-
-
-/*
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  static const String title = "We Rate Dogs";
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+  static const String windowTitle = "Sawors Test";
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
+    ThemeData theme = ThemeData.dark();
+
     return MaterialApp(
-      title: title,
-      theme: ThemeData(
-        brightness: Brightness.dark
-      ),
-      home: const MyHomePage(title: title),
+      title: MainApp.windowTitle,
+      theme: theme,
+      color: Colors.green,
+      home: const HomePage(title: MainApp.windowTitle,),
     );
   }
 }
 
 
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
   final String title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
 
 
-class _MyHomePageState extends State<MyHomePage> {
-  List<Dog> initialDogs = [
-    Dog("Ruby", "Geneva, GE, Switzerland", "Ruby is a very good doggo, a bit annoying tho"),
-    Dog("Rex", "Sion, VS, Switzerland", "Absolute killer, has one time almost caught a legless rabbit"),
-    Dog("Rod", "Lausanne, VD, Switzerland", "Very good dog. Likes to eat Vacherin and drink a lot of undrinkable wine"),
-    Dog("Bob", "Payerne, VD, Switzerland", "Bob is my dog, and my dog is Bob. I like my dog like I like my life. Bob4tw"),
-    Dog("Donkey", "Zurich, ZH, Switzerland", "Absolute idiot"),
-    Dog("Doug", "Paradise Falls, ??, Somewhere in South America", "Very talkative, no really, this dog can talk. Despite his adorable face this filthy bastard can commit the worst treason"),
-    Dog("Bonker", "Austin, TX, United-States", "Bonker is lit !!!!!!! :fire: :fire: :fire: Gigachad :stone_face: :stone_face: :gorilla: :gorilla:")
-  ]
+class _HomePageState extends State<HomePage> {
 
-
-
-
-
-
-
-  ;
-
+  static final Color highlightColor = Colors.indigo.shade700;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: DogCard(initialDogs[1]),
-      )
-    );
+      return SafeArea(
+          child: Scaffold(
+              body: Center(
+                  child: Card(
+                    child: Stack(
+                      children: [
+                        Wrap(
+                          direction: Axis.vertical,
+                          clipBehavior: Clip.hardEdge,
+                          children: [...generateCards(randomBetween(15, 30))],
+                        )
+                      ],
+                    ),
+                  ),
+              )
+          ),
+      );
+  }
+
+
+  List<Card> generateCards(int amount) {
+    List<Card> outputList = [];
+    for(int i = 0; i<amount; i++){
+      outputList.add(
+        Card(
+        color: highlightColor,
+        child: Padding(
+          padding: const EdgeInsetsDirectional.only(
+              start: 12,
+              end: 12,
+              top: 6,
+              bottom: 6
+            ),
+          child: Text(randomAlpha(randomBetween(6, 10))),
+          ),
+        ),
+      );
+    }
+
+
+    return outputList;
   }
 }
-*/
